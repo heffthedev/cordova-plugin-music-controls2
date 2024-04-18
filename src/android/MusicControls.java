34325b4d0e565a8968879b1542844ed49bcf3894
heffthedev <62308758+heffthedev@users.noreply.github.com>
2024-04-18 17:12:37 +0200

    Update MusicControls.java FLAG_IMMUTABLE

--- src/android/MusicControls.java
+++ src/android/MusicControls.java
@@ -149,7 +149,7 @@ public class MusicControls extends CordovaPlugin {
 			Intent headsetIntent = new Intent("music-controls-media-button");
 			this.mediaButtonPendingIntent = PendingIntent.getBroadcast(
 				context, 0, headsetIntent,
-				Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ? PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE : PendingIntent.FLAG_UPDATE_CURRENT
+				Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ? PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE : PendingIntent.FLAG_UPDATE_CURRENT
 			);
 			this.registerMediaButtonEvent();
 		} catch (Exception e) {
